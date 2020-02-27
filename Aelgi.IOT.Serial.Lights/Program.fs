3d09c53369835716f27ea3dda6afbe01304c0c18
sanchez <dfitz360@gmail.com>
2020-02-27 22:03:45 +1000

    Shows the skyblue color on load

--- Aelgi.IOT.Serial.Lights/Program.fs
+++ Aelgi.IOT.Serial.Lights/Program.fs
@@ -1,11 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Drawing
+open Aelgi.IOT.Serial.Lights.Core
 open Aelgi.IOT.Serial.Lights
 
 let rec initialSerial () =
+    printf "Loading available serial ports..."
     let ports = SerialWriter.getAvailablePorts ()
-    printfn "Available Serial Ports:"
+    printfn "\rAvailable Serial Ports:"
     ports |> List.iteri (printfn " - %d) %s")
     
     let pressedNumber =
@@ -18,13 +21,23 @@ let rec initialSerial () =
     match selectedPort with
     | Some p ->
         printfn "Selected: %s" p
+        SerialWriter.connectToPort p
     | None ->
         printfn "Failed to find port!"
         initialSerial()
         
+let initialVisualisation () =
+    printfn "Visualisation mode enabled..."
+        
+let generateOpening (stripCount: int) =
+    [Reset]@[
+        for _ = 1 to stripCount do
+            yield Color.SkyBlue |> Color
+    ]@[Show]
 
 [<EntryPoint>]
 let main argv =
+    let stripCount = 19
     let isDevelopment =
         "DOTNET_ENVIRONMENT"
         |> Environment.GetEnvironmentVariable
@@ -32,7 +45,19 @@ let main argv =
             | "Development" -> true
             | _ -> false
             
-    let serial = initialSerial ()
+    let writeAction =
+        match isDevelopment with
+        | true ->
+            initialVisualisation()
+            LightsVisualizer.handleAction
+        | false ->
+            let serial = initialSerial()
+            SerialWriter.handleAction serial
+    let writer =
+        List.iter writeAction
+        
+    generateOpening stripCount
+    |> writer
     
     printfn "Hello World from F#!"
     0 // return an integer exit code
